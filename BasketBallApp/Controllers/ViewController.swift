e7676274d0e3d6337948ffa96360340e1cbf64a0
crowjambo <evaldascrow@gmail.com>
2020-01-08 17:01:14 +0200

    UICollection view implementation +shadows

--- BasketBallApp/Controllers/ViewController.swift
+++ BasketBallApp/Controllers/ViewController.swift
@@ -1,12 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+	
 	//main collection (could be database or in another file)
 	var teams: [TeamInfo] = []
 	
-	//get tableView so we can call its reload method later for updating UI
-	@IBOutlet weak var tableViewOutlet: UITableView!
 	
 	// default controller function
 	override func viewDidLoad() {
@@ -15,29 +13,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 		
 		// move all of this to separate model file later
 		teams.append(TeamInfo(teamName: "test", description: "adsasdasdasd"))
+		teams.append(TeamInfo(teamName: "test2", description: "a234dasd"))
+		teams.append(TeamInfo(teamName: "test3", description: "adsas234sd"))
+		teams.append(TeamInfo(teamName: "test", description: "adsasd234dasd"))
+		
+		
 		
 		
 	}
 	
-	//set custom values and init the cell (on each reload) (MAIN CELL CREATION FUNCTION)
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-		let cell = tableView.dequeueReusableCell(withIdentifier: "TeamInfoCell", for: indexPath) as! TeamInfoTableCell
-		
-		// set that new cell values
-		cell.titleTeamNameOutlet.text = teams[indexPath.row].teamName
-		cell.teamDescriptionOutlet.text = teams[indexPath.row].description
-		
-		
-		
-		// one cell is created
-		return cell
-	}
 	
-	//number of items in tableView (helper function for implementation of UITableView)
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		return teams.count
 	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TeamCollectionCell", for: indexPath) as! TeamCollectionCell
+		
+		cell.teamNameOutlet.text = teams[indexPath.row].teamName
+		cell.teamDescriptionOutlet.text = teams[indexPath.row].description
+		
+		cell.contentView.layer.cornerRadius = 2.0
+		cell.contentView.layer.borderWidth = 1.0
+		cell.contentView.layer.borderColor = UIColor.clear.cgColor
+		cell.contentView.layer.masksToBounds = true
+
+		cell.layer.shadowColor = UIColor.black.cgColor
+		cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+		cell.layer.shadowRadius = 2.0
+		cell.layer.shadowOpacity = 0.5
+		cell.layer.masksToBounds = false
+		cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+		
+		return cell
+	}
+
 	
 
 }
---
+++ BasketBallApp/Views/TeamCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  TeamCollectionCell.swift
+//  BasketBallApp
+//
+//  Created by Evaldas on 1/8/20.
+//  Copyright © 2020 Evaldas. All rights reserved.
+//
+
+import UIKit
+
+class TeamCollectionCell: UICollectionViewCell {
+    
+	@IBOutlet weak var teamIconOutlet: UIImageView!
+	@IBOutlet weak var teamNameOutlet: UILabel!
+	@IBOutlet weak var teamDescriptionOutlet: UILabel!
+	
+	
+}
