51161cbf2d411265fb2ba091d01c551a5f1dc170
crowjambo <evaldascrow@gmail.com>
2020-01-15 16:43:00 +0200

    some unit tests for async requests

--- BasketBallAppTests/BasketBallAppTests.swift
+++ BasketBallAppTests/BasketBallAppTests.swift
@@ -11,72 +11,52 @@ class BasketBallAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+	func test_getRequest_loadPlayers_not_null(){
+		var playerCol:[Player]?
+		let expectation = self.expectation(description: "loading")
 		
-		let someVar = "Hello"
+		NetworkAccess.getPlayers(teamName: "Atlanta", completionHandler: {(players) in
+			playerCol = players
+			
+			expectation.fulfill()
+		})
 		
-		XCTAssertEqual("Hello", someVar)
-    }
-	
-	
-	
-	func test_MatchHistory_gibberish_team1name_init(){
-		let name = "testasbd1123[]23#@#@#absd"
-		
-		let mh = MatchHistory(team1Name: name, team2Name: nil, date: nil)
-		
-		XCTAssertEqual(mh.team1Name, name)
-		
+		waitForExpectations(timeout: 5, handler: nil)
+		XCTAssertNotNil([playerCol])
 	}
 	
-	func test_MatchHistory_gibberish_team2name_init(){
-		let name = "tesdfssdf234123[]///asdzxc..."
+	func test_getRequest_loadMatches_not_null(){
+		var matchCollection:[MatchHistory]?
+		let expectation = self.expectation(description: "loading")
 		
-		let mh = MatchHistory(team1Name: nil, team2Name: name, date: nil)
+		NetworkAccess.getMatches(teamID: "134881", completionHandler: { (matches) in
+			matchCollection = matches
+			
+			expectation.fulfill()
+			
+		})
 		
-		XCTAssertEqual(mh.team2Name, name)
+		waitForExpectations(timeout: 5, handler: nil)
+		XCTAssertNotNil(matchCollection)
 	}
 	
-	func test_MatchHistory_gibberish_date_init(){
-		let name = "tesdfssdf234123[]///asdzxc..."
+	func test_getRequest_loadTeams_not_null(){
 		
-		let mh = MatchHistory(team1Name: nil, team2Name: nil, date: name)
+		var teamsCollection:[TeamInfo]?
+		let expectation = self.expectation(description: "loading")
 		
-		XCTAssertEqual(mh.date, name)
+		NetworkAccess.getTeams(completionHandler: { (teams) in
+			teamsCollection = teams
+
+			expectation.fulfill()
+		})
+		
+		XCTAssertNotNil(teamsCollection)
 	}
 	
-	func test_teams_appending(){
-		var teams: [TeamInfo] = []
-		teams.append(TeamInfo(teamName: "test", description: "test", imageIconName: "square", imageTeamMain: "test"))
-		teams.append(TeamInfo(teamName: "test", description: "test", imageIconName: "square", imageTeamMain: "test"))
-		teams.append(TeamInfo(teamName: "test", description: "test", imageIconName: "square", imageTeamMain: "test"))
-		
-		XCTAssertEqual(3, teams.count)
-	}
-	
-	
-	func test_LoadingDataFromDatabase(){
-		let data = TeamsDatabase.LoadFakeData()
-		
-		XCTAssertNotNil(data)
-	}
-	
-	func test_loadedFakeDataIsRequiredType(){
-		let data = TeamsDatabase.LoadFakeData()
-	
-		
-		XCTAssertTrue(data is [TeamInfo])
-	}
 
 	
-	
-    func testPerformanceExample() {
-        // This is an ex ample of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
 
 }
